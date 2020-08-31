e1e1d8c804341147bc17745329b1ba00d185f4dc
caioslppuo <CaioslppUO@gmail.com>
2020-08-30 23:11:39 -0300

    Correçao script carregar arquivo config 4

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import json,os,time
 const_script_folder: str = ''
 const_script_name: str = ''
 const_server_config_file: str = ''
+const_server_configs = None
 
 def load_config_files():
     global const_script_folder,const_script_name
@@ -11,9 +12,10 @@ def load_config_files():
         const_script_folder = data['script_location']
         const_script_name = data['script_name']
 
-    global const_server_config_file
+    global const_server_config_file,const_server_configs
     with open('server_config.json') as json_file:
         data = json.load(json_file)
+        const_server_configs = data
         const_server_config_file = data['server_config_file']
 
 def run_server():
@@ -36,15 +38,31 @@ def listen_commands():
             time.sleep(4)
         command: str = input("Digite o comando:")
 
+def set_server_configs():
+    command: str = ""
+    command += str("XPMultiplier=" + const_server_configs['XPMultiplier'] + "\n")
+    return command
+
+def write_server_config_file(file_content):
+    with open(const_server_config_file, 'w') as file:
+        file.write(file_content)
+
 def process_server_config_file():
+    command = ""
+    flag = True
     with open(const_server_config_file, 'r') as file:
         lines = file.read().splitlines()
-        last = ''
         for line in lines:
+            command += (line + "\n")
+            if(line == "[ServerSettings]"):
+                flag = False
             if(line == ""):
-                print(last)
-                break
-            last = line
+                flag = True
+            if(flag == True):
+                command += set_server_configs()
+                flag = False
+    if(command != ""):
+        write_server_config_file(command)
 
 if __name__ == "__main__" :
     try:
